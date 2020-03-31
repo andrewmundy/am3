c1610e8a10f1535d4b66b6d001e9de910f85e1ba
Andrew Mundy <andrewmundy@gmail.com>
2020-03-31 15:44:46 -0700

    added name

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const Home = () => (
   <div className="container">
     <h1 className="mint">Andrew Mundy</h1>
+    <h1 className="roto">Andrew Mundy</h1>
     <h1 className="roto">Andrew Mundy</h1>
     <style jsx>{``}</style>
 
@@ -13,61 +14,60 @@ const Home = () => (
         margin: 0;
       }
       h1.mint {
-        font-family: 'MintGrotesk-Bold';
-        font-size:100px;
+        font-family: "MintGrotesk-Bold";
+        font-size: 100px;
       }
-      h1.roto{
-        font-family: 'Rotor-0';
-        font-size:100px;
+      h1.roto {
+        font-family: "Rotor-0";
+        font-size: 100px;
       }
       .container {
-        padding:100px;
+        padding: 100px;
       }
 
       h1 {
-        font-variation-settings: 'style' Rotor-0;
+        font-variation-settings: "style" Rotor-0;
         animation: breathe 4000ms infinite forwards;
       }
-      
+
       @keyframes breathe {
         60% {
-          font-variation-settings: 'style' Rotor-90
+          font-variation-settings: "style" Rotor-90;
         }
-      
+
         100% {
-          font-variation-settings: 'style' Rotor-180
+          font-variation-settings: "style" Rotor-180;
         }
       }
       * {
         box-sizing: border-box;
       }
       @font-face {
-        font-family: 'MintGrotesk-Black';
-            src: url('/fonts/Mint-Grotesk/MintGroteskTrialV0.7-Black.otf');
+        font-family: "MintGrotesk-Black";
+        src: url("/fonts/Mint-Grotesk/MintGroteskTrialV0.7-Black.otf");
       }
       @font-face {
-        font-family: 'MintGrotesk-Bold';
-            src: url('/fonts/Mint-Grotesk/MintGroteskTrialV0.7-Bold.otf');
+        font-family: "MintGrotesk-Bold";
+        src: url("/fonts/Mint-Grotesk/MintGroteskTrialV0.7-Bold.otf");
       }
       @font-face {
-        font-family: 'MintGrotesk-Medium';
-            src: url('/fonts/Mint-Grotesk/MintGroteskTrialV0.7-Medium.otf');
+        font-family: "MintGrotesk-Medium";
+        src: url("/fonts/Mint-Grotesk/MintGroteskTrialV0.7-Medium.otf");
       }
       @font-face {
-        font-family: 'Rotor-0';
-            src: url('/fonts/Rotor/rotor-trial-0.otf');
+        font-family: "Rotor-0";
+        src: url("/fonts/Rotor/rotor-trial-0.otf");
       }
       @font-face {
-        font-family: 'Rotor-90';
-            src: url('/fonts/Rotor/rotor-trial-90.otf');
+        font-family: "Rotor-90";
+        src: url("/fonts/Rotor/rotor-trial-90.otf");
       }
       @font-face {
-        font-family: 'Rotor-180';
-            src: url('/fonts/Rotor/rotor-trial-180.otf');
+        font-family: "Rotor-180";
+        src: url("/fonts/Rotor/rotor-trial-180.otf");
       }
-
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
