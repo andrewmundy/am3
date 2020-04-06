63da9510531950604d7f2fb16753141fb57384e4
Andrew Mundy <andrewmundy@gmail.com>
2020-04-06 11:44:57 -0700

    name

--- pages/api/send.js
+++ pages/api/send.js
@@ -3,11 +3,11 @@ const sgMail = require('@sendgrid/mail')
 export default async function(req, res) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-  const { email, message, name } = req.body
+  const { email, message} = req.body
   const content = {
     to: 'andrewmundy@gmail.com',
     from: email,
-    subject: `New Message From - ${name}`,
+    // subject: `New Message From - ${name}`,
     text: message,
     html: `<p>${message}</p>`
   }
